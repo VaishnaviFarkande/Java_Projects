c1d139379fafcab94c767af0ac13248fcc693e9f
VaishnaviFarkande <89068896+VaishnaviFarkande@users.noreply.github.com>
2021-09-06 00:34:26 -0700

    Assignment 1
    Assignment 1

---
+++ Assignment 1/StaticExample.java
@@ -0,0 +1,17 @@
+public class StaticExample
+{
+public static void main(String[] args)
+{
+System.out.println("In main method");
+method1();
+method2();
+}
+public static void method1()
+{
+System.out.println("method1");
+}
+public static void method2()
+{
+System.out.println("method2");
+}
+}
