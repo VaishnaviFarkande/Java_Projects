ec4ca45af9812c9e8f9a9d121e7b7fb92e6a2fb2
VaishnaviFarkande <89068896+VaishnaviFarkande@users.noreply.github.com>
2021-09-06 00:53:19 -0700

    Assignment 3
    Assignment 3

---
+++ Assignment 3/AsciiValue.java
@@ -0,0 +1,13 @@
+/*Java Program to Find ASCII Value of a Character*/
+
+public class AsciiValue
+{
+public static void main(String[] args) 
+{
+char ch = 'a';
+int ascii = ch;
+int castAscii = (int) ch;
+System.out.println("The ASCII value of " + ch + " is: " + ascii);
+System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+}
+}
